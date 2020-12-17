bfd4d12ecbe334f57a0f0af8e6cf19ab2520f34e
SaiSakethAluru <alurusaisaketh@gmail.com>
2020-12-17 16:33:10 +0530

    minor fixes in encoder layers

--- src/sent_encoder.py
+++ src/sent_encoder.py
@@ -29,6 +29,8 @@ class SentenceEncoder(nn.Module):
             max_length,
             labels
         )
+        ## TODO: Make this pretrained from glove
+        self.word_embedding = nn.Embedding(max_length,embed_size)   # Needed to get the label embeddings
         self.position_embedding = nn.Embedding(max_length,embed_size)
         self.layers = nn.ModuleList(
             [
@@ -41,6 +43,26 @@ class SentenceEncoder(nn.Module):
         self.dropout = nn.Dropout(dropout)
     def forward(self,x,mask):
         N,par_len,seq_len = x.shape
-        positions = torch.arange(0,par_len).expand(N,par_len)
+        positions = torch.arange(0,par_len).expand(N,par_len).to(self.device)
+        word_level_outputs = []
+        for i,sent in enumerate(x):
+            word_level_outputs.append(
+                self.word_level_encoder(
+                    sent.reshape(N,seq_len), mask[i].reshape(N,seq_len)
+                )
+            )
+        # NOTE: shape of each output tensor here should be N,embed_size for each entry of the above list
+        # NOTE: After stacking it should be N,par_len, embed_size if everything works fine. Else adjust it.
+        word_level_outputs = torch.stack(word_level_outputs,dim=1)
+        out = self.dropout(
+            (word_level_outputs + self.position_embedding(positions))
+        )
+        label_embed = [
+            self.word_embedding(label) for label in self.labels
+        ]
+        # NOTE: Each entry in the above list should be 1,embed_size. If not adjust to this size
+        label_embed = torch.cat(label_embed,dim=0)
+        for layer in self.layers:
+            out = layer(out,out,out,mask)
 
 
--- src/word_encoder.py
+++ src/word_encoder.py
@@ -18,7 +18,8 @@ class WordEncoder(nn.Module):
         super(WordEncoder,self).__init__()
         self.embed_size = embed_size
         self.device = device
-        # Include glove here. 
+        self.labels = labels
+        ## TODO: Make this pretrained from glove
         self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
         self.position_embedding = nn.Embedding(max_length,embed_size)
         self.layers = nn.ModuleList(
@@ -37,8 +38,13 @@ class WordEncoder(nn.Module):
         out = self.dropout(
             (self.word_embedding(x)+self.position_embedding(positions))
         )
+        label_embed = [
+            self.word_embedding(label) for label in self.labels
+        ]
+        # NOTE: Each entry in the above list should be 1,embed_size. If not adjust to this size
+        label_embed = torch.cat(label_embed,dim=0)
         for layer in self.layers:
-            out = layer(out,out,out,mask)
+            out = layer(out,out,out,label_embed,mask)
         
         return out
     
