51886ec91ec375a733f1d0cb4fa3954a0da47700
SaiSakethAluru <alurusaisaketh@gmail.com>
2020-12-17 15:55:26 +0530

    word and sent encoders

--- src/sent_encoder.py
+++ src/sent_encoder.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 from encoder_transformer_block import EncoderTransformerBlock
-
-class WordEncoder(nn.Module):
+from word_encoder import WordEncoder
+class SentenceEncoder(nn.Module):
     def __init__(
-        self, 
+        self,
         src_vocab_size, 
         embed_size, 
         num_layers, 
@@ -15,30 +15,32 @@ class WordEncoder(nn.Module):
         max_length,
         labels
     ):
-        super(WordEncoder,self).__init__()
+        super(SentenceEncoder,self).__init__()
         self.embed_size = embed_size
         self.device = device
-        # Include glove here. 
-        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.word_level_encoder = WordEncoder(
+            src_vocab_size, 
+            embed_size, 
+            num_layers, 
+            heads, 
+            device, 
+            forward_expansion, 
+            dropout, 
+            max_length,
+            labels
+        )
         self.position_embedding = nn.Embedding(max_length,embed_size)
         self.layers = nn.ModuleList(
             [
                 EncoderTransformerBlock(
-                    embed_size,heads,dropout,forward_expansion,labels    
+                     embed_size, heads, dropout, forward_expansion, labels
                 )
                 for _ in range(num_layers)
             ]
         )
         self.dropout = nn.Dropout(dropout)
+    def forward(self,x,mask):
+        N,par_len,seq_len = x.shape
+        positions = torch.arange(0,par_len).expand(N,par_len)
 
-    def forward(self, x, mask):
-        N,seq_len = x.shape
-        positions = torch.arange(0,seq_len).expand(N,seq_len).to(self.device)
-        out = self.dropout(
-            (self.word_embedding(x)+self.position_embedding(positions))
-        )
-        for layer in self.layers:
-            out = layer(out,out,out,mask)
-        
-        return out
-    
+
