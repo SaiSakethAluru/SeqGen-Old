748c443a6ef1e1e6baac984f1598107d26611c90
SaiSakethAluru <alurusaisaketh@gmail.com>
2020-12-19 15:17:49 +0530

    decoder blocks priliminary code

---
+++ src/decoder_block.py
@@ -0,0 +1,20 @@
+import torch 
+import torch.nn as nn
+from selfatt import SelfAttention
+from decoder_transformer_block import DecoderTransformerBlock
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
+        super(DecoderBlock, self).__init__()
+        self.norm = nn.LayerNorm(embed_size)
+        self.attention = SelfAttention(embed_size, heads=heads)
+        self.transformer_block = DecoderTransformerBlock(
+            embed_size, heads, dropout, forward_expansion
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, encoder_word_level_output, src_mask, trg_mask):
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention + x))
+        out = self.transformer_block(value, key, query, encoder_word_level_output, src_mask)
+        return out
---
+++ src/decoder_transformer_block.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from selfatt import SelfAttention
+
+class DecoderTransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(DecoderTransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.norm3 = nn.LayerNorm(embed_size)
+        self.enc_sent_attention = SelfAttention(embed_size,heads)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size),
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, encoder_word_level_output, mask):
+        #encoder_word_level should be N,embed_size
+        #value,key,query should be?
+        attention = self.attention(value, key, query, mask)
+
+        # Add skip connection, run through normalization and finally dropout
+        x = self.dropout(self.norm1(attention + query))
+        # include word level outputs of encoder here
+        encoder_word_attentions = self.enc_sent_attention(encoder_word_level_output,encoder_word_level_output,x,mask)
+        x = self.dropout(self.norm3(x+encoder_word_attentions))
+
+
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
